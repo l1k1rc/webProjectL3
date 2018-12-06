b5503824af012e1094378a4937a5ec1ffa6cba21
l1k1rc <45100859+l1k1rc@users.noreply.github.com>
2018-12-06 18:33:27 +0100

    Add files via upload

--- location.php
+++ location.php
@@ -2,7 +2,7 @@
     require('registerFunctions.php');
     function loclogin(){
         if(!empty($_SESSION['login'])){
-           return '<input type="text" name="email" required="" value="'.$_SESSION['login'].'">';
+           return '<input type="text" id="email" name="email" required="" value="'.$_SESSION['login'].'">';
         }
     }
    
@@ -69,7 +69,7 @@
                 <form enctype="multipart/form-data" action="registerFunctions.php" method="POST">
                     <div id="leftSide">
                         <label for="type">Veuillez choisir la catégorie de votre voiture :</label><br />
-                        <select name="type">
+                        <select name="type" id="type">
                             <option value="citadine">Citadine</option>
                             <option value="suv">SUV</option>
                             <option value="4x4">4x4</option>
@@ -80,40 +80,40 @@
                         </select>
                         <br />
                         <label for="couleur">Veuillez saisir la couleur de votre voiture :</label><br />
-                        <input type="color" name="couleur">
+                        <input type="color" name="couleur" id="couleur">
                         <br />
                         <label for="marque">Veuillez saisir la marque de votre voiture :</label><br />
-                        <input type="text" name="marque" required="">
+                        <input type="text" name="marque" required="" id="marque">
                         <br />
                         <label for="kilometer">Veuillez saisir le kilométrage de votre voiture :</label><br />
-                        <input type="text" name="kilometer" required="">
+                        <input type="text" name="kilometer" required="" id="kilometer">
                         <br />
                         <label for="modele">Veuillez saisir le modèle de votre voiture :</label><br />
-                        <input type="text" name="modele" required="">
+                        <input type="text" name="modele" required="" id="modele">
                         <br />
                         <label for="titleloc">Veuillez saisir le titre de votre location :</label><br />
-                        <input type="text" name="titleloc" required="">
+                        <input type="text" name="titleloc" required="" id="titleloc">
                         <br />
                         <label for="places">Veuillez saisir le nombre de places :</label><br />
-                        <input type="number" name="places" min="1" max="7" required="">
+                        <input type="number" name="places" min="1" max="7" required="" id="places">
                         <br />
                     </div>
                     <div id="rightSide">
                         <label for="portes">Veuillez saisir le nombre de portes :</label><br />
-                        <input type="number" name="portes" min="1" max="5" required="">
+                        <input type="number" name="portes" min="1" max="5" required="" id="portes">
                         <br />
                         <label for="puissance">Veuillez saisir la puissance de la voiture :</label><br />
-                        <input type="text" name="puissance" required="">
+                        <input type="text" name="puissance" required="" id="puissance">
                         <br />
                         <label for="boite">Veuillez saisir le type de la boite de vitesse :</label><br />
-                        <select name="boite">
+                        <select name="boite" id="boite">
                             <option value="auto">Automatique</option>
                             <option value="manuelle">Manuelle</option>
                             <option value="semiauto">Semi-automatique</option>
                         </select>
                         <br />
                         <label for="carburant">Veuillez choisir le type de carburant de la voiture :</label><br />
-                        <select name="carburant">
+                        <select name="carburant" id="carburant">
                             <option value="essence">Essence</option>
                             <option value="diesel">Diesel</option>
                             <option value="electrique">Electrique</option>
@@ -124,25 +124,25 @@
                         <?php echo loclogin(); ?>
                         <br />
                         <label for="duree">Veuillez saisir la durée de location :</label><br />
-                        <input type="number" name="duree" min="1" max="12" required="">
+                        <input type="number" name="duree" min="1" max="12" required="" id="duree">
                         <label>semaines</label><br />
                         <br />
                         <label for="prix">Veuillez saisir le prix de la location :</label><br />
-                        <input type="text" name="prix" required="">
+                        <input type="text" name="prix" required="" id="prix">
                         <label>/jour</label><br />
                         <br />
                     </div><br />
                     <div id="filePart">
                         <label for="image"><b>Proposer une photo illustrant votre location :</b></label><br /><br />
-                        <input type="file" accept="image/*" onchange="loadFile(event)" name="image" id="image" required="" style="margin-bottom: 1cm">
+                        <input type="file" accept="image/*" onchange="loadFile(event)" name="image" id="image" required="" style="margin-bottom: 1cm" >
                         <br />
-                        <img id="output" style="max-width: 180px; margin-bottom: 1cm; margin-left: 5cm;" alt=""> 
+                        <img id="output1" style="max-width: 250px; margin-bottom: 1cm; margin-left: 10cm;" alt="" src="#"> 
                         <br />
                         <script>var loadFile = function(event) {
                             var reader = new FileReader();
                             reader.onload = function()
                             {
-                                var output = document.getElementById('output');
+                                var output = document.getElementById('output1');
                                 output.src = reader.result;
                             };
                             reader.readAsDataURL(event.target.files[0]);
@@ -153,13 +153,13 @@
                         <br />
                         <input type="file" accept="image/*" onchange="loadFile(event)" name="image1" id="image1" required="" style="margin-bottom: 1cm">
                         <br />
-                        <img id="output" style="max-width: 180px; margin-bottom: 1cm; margin-left: 5cm;" alt=""> 
+                        <img id="output2" style="max-width: 250px; margin-bottom: 1cm; margin-left: 10cm;" alt="" src="#"> 
                         <br />
                         <script>var loadFile = function(event) {
                             var reader = new FileReader();
                             reader.onload = function()
                             {
-                                var output = document.getElementById('output');
+                                var output = document.getElementById('output2');
                                 output.src = reader.result;
                             };
                             reader.readAsDataURL(event.target.files[0]);
@@ -167,13 +167,13 @@
                          </script>
                          <input type="file" accept="image/*" onchange="loadFile(event)" name="image2" id="image2" required="" style="margin-bottom: 1cm">
                         <br />
-                        <img id="output" style="max-width: 180px; margin-bottom: 1cm; margin-left: 5cm;" alt=""> 
+                        <img id="output3" style="max-width: 250px; margin-bottom: 1cm; margin-left: 10cm;" alt="" src="#"> 
                         <br />
                         <script>var loadFile = function(event) {
                             var reader = new FileReader();
                             reader.onload = function()
                             {
-                                var output = document.getElementById('output');
+                                var output = document.getElementById('output3');
                                 output.src = reader.result;
                             };
                             reader.readAsDataURL(event.target.files[0]);
@@ -181,13 +181,13 @@
                          </script>
                          <input type="file" accept="image/*" onchange="loadFile(event)" name="image3" id="image3" required="" style="margin-bottom: 1cm">
                         <br />
-                        <img id="output" style="max-width: 180px; margin-bottom: 1cm; margin-left: 5cm;" alt=""> 
+                        <img id="output4" style="max-width: 250px; margin-bottom: 1cm; margin-left: 10cm;" alt="" src="#"> 
                         <br />
                         <script>var loadFile = function(event) {
                             var reader = new FileReader();
                             reader.onload = function()
                             {
-                                var output = document.getElementById('output');
+                                var output = document.getElementById('output4');
                                 output.src = reader.result;
                             };
                             reader.readAsDataURL(event.target.files[0]);
